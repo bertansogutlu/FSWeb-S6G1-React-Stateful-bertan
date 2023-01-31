4a3ae7bcd61b7a64e5db6abab9a000253981adcf
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-01-31 19:29:55 +0300

    Dondurucu bitti

--- src/components/Dondurucu.js
+++ src/components/Dondurucu.js
@@ -35,23 +35,24 @@ ADIM 4:
   "not" için kullandığımız operatörü hatırlıyor musunuz??
 */
 
-import React from 'react'; /* STEP 0 */
+import React, { useState } from 'react'; /* STEP 0 */
 
 export default function Dondurucu() {
 /* ADIM 1 */
-
+const [donduruAcik, setDonduruAcik] = useState(true);
   const toggleDondurucu = () => {
   /* ADIM 4 */
+  setDonduruAcik(!donduruAcik)
   };
 
   return (
     <div className='widget-spinner container'>
       <h2>Döndürücü</h2>
       {
-        true && <div id='döndürücü' className='spinner'>--+--</div> /* ADIM 2 */
+        true && donduruAcik && <div id='döndürücü' className='spinner'>--+--</div> /* ADIM 2 */
       }
       <button id='toggleDondurucu' onClick={toggleDondurucu}>
-         Gizle {/* STEP 3 */}
+         { donduruAcik ? 'Gizle' : 'Goster' }
       </button>
     </div>
   );
